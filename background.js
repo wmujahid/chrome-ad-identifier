e1b0c4372b6a2e70e2b6a3d18763cf539e8e46b0
wmujahid <wmujahid47@gmail.com>
2022-03-16 16:35:26 -0400

    on extension click all ads under 'taw' id are highlighted red

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
-function reddenPage() {
-  document.body.style.backgroundColor = 'red';
+// function reddenPage() {
+//   document.body.style.backgroundColor = 'red';
+// }
+function reddenAds() {
+  let ads = document.getElementById('taw');
+  ads.style.backgroundColor = "red";
+  console.log('Ads should be red')
 }
 
 chrome.action.onClicked.addListener((tab) => {
   if(!tab.url.includes("chrome://")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      function: reddenPage
+      // function: reddenPage,
+      function: reddenAds
     });
   }
 });
